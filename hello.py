3096dced4f37067e5e0e14fad56b41b0ae07d2fd
Perrin Samuels <perrin@aristotle>
2022-08-04 09:57:56 -0700

    show total num messages

--- hello.py
+++ hello.py
@@ -1,3 +1,13 @@
 #!/usr/bin/env python
-print("Hello, from Messagediscovery")
+
+import sqlite3
+
+print("Hello, from Messagediscovery.")
+conn = sqlite3.connect("chat.db")
+cursor = conn.cursor()
+cursor.execute("select count(*) from message")
+rows = cursor.fetchall()
+cursor.close()
+count = rows[0][0]
+print(f"I found {count} total messages.")
 
